e7914c4389718b32e0f37d253e2d4408c6a3a4e0
Harsh <harshgajjar145@gmail.com>
2023-11-29 12:46:41 +0530

    fix: build fixes

--- hooks/useBridge.tsx
+++ hooks/useBridge.tsx
@@ -55,6 +55,7 @@ export default function useBridge() {
 
       console.log(messagedata);
 
+      //@ts-ignore
       const message = messagedata?.args?.message;
 
       return { message, transactionHash };
