b2e330065ef69947231cd724d4d4dd5a5c010fb8
Shivam <alekarshivam23@gmail.com>
2023-12-12 00:12:16 +0530

    Update getChainName.ts

--- utils/getChainName.ts
+++ utils/getChainName.ts
@@ -1,6 +1,6 @@
 import { chains } from "@/lib/data";
 
 export default function getChainName(chainID: number) {
-    const chain = chains.find((chain) => chain.chainId == chainID);
+    const chain = chains.find((chain) => chain.testnetChainId == chainID);
     return chain?.name
 }
