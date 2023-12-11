5c85de268fef6ae3696428019e83bc4d8c046076
Shivam <alekarshivam23@gmail.com>
2023-12-11 21:20:11 +0530

    feat: changed attestation api

--- utils/getAttestation.ts
+++ utils/getAttestation.ts
@@ -1,7 +1,7 @@
 const getAttestation = async (messageHash: string) => {
   try {
     const _response = await fetch(
-      `https://iris-api-sandbox.circle.com/attestations/${messageHash}`
+      `https://iris-api.circle.com/attestations/${messageHash}`
     );
     const response = await _response.json();
     console.log("Attestation response: ", response);
