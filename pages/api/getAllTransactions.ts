b806f7de9a535c0ab4789fbce846519f69ab2d5e
Harsh <harshgajjar145@gmail.com>
2023-12-11 20:03:02 +0530

    fea: db changes and testnet

--- pages/api/getAllTransactions.ts
+++ pages/api/getAllTransactions.ts
@@ -9,6 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
             const transactions = await prisma.transaction.findMany({
                 where: {
                     sender: address,
+                    testnet: false
                 },
             });
 
